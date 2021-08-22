ce235cdc652ce301afe582a6937844283eccb256
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-08-22 10:17:51 +0530

    Create welcome.spec.js

---
+++ integrations/welcome.spec.js
@@ -0,0 +1,32 @@
+// welcome_page_spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe('example to-do app', () => {
+
+    it('visit test site',() => {
+        cy.visit('https://example.cypress.io/todo')
+    })
+
+    it('displays logo of the company', () => {
+        cy.get('[class=header]').should('be.visible');
+    })
+
+
+  })
