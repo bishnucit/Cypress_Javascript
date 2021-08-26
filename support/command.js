2af85b9ce04ab2dbcf66d0c0a63b5b64f0af7193
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-08-26 22:03:42 +0530

    Update command.js

--- support/command.js
+++ support/command.js
@@ -1,3 +1,3 @@
 Cypress.Commands.add('waitForSpinners', () => {
-  cy.get('.loader__container', { timeout: 10_000 }).should('have.length', 0)
+  cy.get('.spinner', { timeout: 10_000 }).should('have.length', 0)
 })
