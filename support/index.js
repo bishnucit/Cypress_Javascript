e1ec2543012e6a3c2c045978c6a5ae31b64fe9af
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-08-28 13:11:11 +0530

    Update index.js

--- support/index.js
+++ support/index.js
@@ -1,4 +1,14 @@
 import './commands'
+
 Cypress.on('uncaught:exception', (err, runnable) => {
   return false
+})
+
+Cypress.on('fail', (error, runnable) => {
+  debugger
+
+  // we now have access to the err instance
+  // and the mocha runnable this failed on
+
+  throw error // throw error to have test still fail
 })
