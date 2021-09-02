afa71192d57aeaecf022c128197d682d4de7c827
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-09-02 16:56:53 +0530

    Update command.js

--- support/command.js
+++ support/command.js
@@ -1,3 +1,34 @@
-Cypress.Commands.add('waitForSpinners', () => {
-  cy.get('.spinner', { timeout: 10_000 }).should('have.length', 0)
-})
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+  Cypress.Commands.add('waitForSpinners', () => {
+    cy.get('.spinner', { timeout: 10_000 }).should('have.length', 0)
+  });
+
+  Cypress.Commands.add("dragTo", { prevSubject: "element" }, (subject, targetEl) => {
+    cy.wrap(subject).trigger("dragstart");
+    cy.get(targetEl).trigger("drop");
+    cy.get(targetEl).trigger("dragend");
+  });
