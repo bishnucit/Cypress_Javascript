83f5673149a690b7a47e46cc0ece98b496d3bbe0
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-09-23 17:26:40 +0530

    Update index.js

--- support/index.js
+++ support/index.js
@@ -1,14 +1,43 @@
+// ***********************************************************
+// This example support/index.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
 import './commands'
+import failOnConsoleError, { consoleType } from "cypress-fail-on-console-error"
 
 Cypress.on('uncaught:exception', (err, runnable) => {
   return false
-})
+});
 
-Cypress.on('fail', (error, runnable) => {
-  debugger
+const config = {
+    excludeMessages: ['firstError', 'secondError'],
+    includeConsoleTypes: [consoleType.ERROR, consoleType.WARN],
+};
 
-  // we now have access to the err instance
-  // and the mocha runnable this failed on
+failOnConsoleError(config);
 
-  throw error // throw error to have test still fail
-})
+Cypress.on("window:before:load", win => {
+  cy.stub(win.console, "error").callsFake(msg => {
+    //ignores known error
+    if (msg.includes("This is an error")) {
+      return null;
+    }
+    // log out to the terminal
+    cy.now("task", "error", msg);
+    // log to Command Log and fail the test
+    throw new Error(msg);
+  });
+});
