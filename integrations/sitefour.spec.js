0c22f9095588ade3d476be231eb34776b6673e09
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-09-29 17:08:37 +0530

    Create sitefour.spec.js

---
+++ integrations/sitefour.spec.js
@@ -0,0 +1,13 @@
+// sitefour.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+//Download the app from https://github.com/cypress-io/cypress-realworld-app 
+
+/// <reference types="cypress"/>
+
+
+const testURL = 'http://localhost:3000';
