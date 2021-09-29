8640e1d42b5da7593c440aea0415f218cc9525e8
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-09-29 17:31:26 +0530

    Update sitefour.spec.js

--- integrations/sitefour.spec.js
+++ integrations/sitefour.spec.js
@@ -5,9 +5,31 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 
-//Download the app from https://github.com/cypress-io/cypress-realworld-app 
+//Download the app from https://github.com/cypress-io/cypress-realworld-app
 
 /// <reference types="cypress"/>
 
 
 const testURL = 'http://localhost:3000';
+
+
+describe('Verify all the elements of the websites are existing', () => {
+
+    beforeEach(() => {
+        cy.visit(testURL,{failOnStatusCode: false});
+    });
+
+    it('TC001 - Sign in page - Verify elements of sign in page', () => {
+
+        cy.get('.makeStyles-logo-3').should('exist');
+        cy.get('.MuiTypography-h5').should('exist');
+        cy.get('#username').should('exist');
+        cy.get('#password').should('exist');
+        cy.get('.MuiFormControlLabel-root > .MuiTypography-root').should('exist');
+        cy.get('.PrivateSwitchBase-input-14').should('exist');
+        cy.get('[data-test=signup]').should('exist');
+        cy.get('[data-test="signin-submit"]').should('exist');
+
+    });
+
+});
