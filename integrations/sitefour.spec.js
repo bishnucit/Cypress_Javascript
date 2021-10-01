c11f1dc96b71226bc395894e98323df03ae7e702
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-10-01 16:51:53 +0530

    Update sitefour.spec.js

--- integrations/sitefour.spec.js
+++ integrations/sitefour.spec.js
@@ -15,6 +15,7 @@ const testURL = 'http://localhost:3000';
 
 
 const LOGIN = () => {
+    //login to app (remember to make a user with tester tester before runing automation)
     cy.get('#username').clear().type('tester');
     cy.get('#password').clear().type('tester');
     cy.get('[data-test=signin-submit]').click();
@@ -22,11 +23,30 @@ const LOGIN = () => {
 };
 
 const LOGOUT = () => {
+    //logout from app
     cy.get('[data-test=sidenav-signout]').should('be.visible');
     cy.get('[data-test=sidenav-signout]').click();
     cy.get('.MuiTypography-h5').should('have.text', 'Sign in');
 };
 
+const BODY = () => {
+    //verify elements of static body of the website
+    cy.get('[data-test=sidenav-username]').should('have.text','@tester');
+    cy.get('[data-test=sidenav-user-balance]').should('have.text', '$0.00');
+    cy.get('[data-test=sidenav-home]').should('exist');
+    cy.get('[data-test=sidenav-user-settings]').should('exist');
+    cy.get('[data-test=sidenav-bankaccounts]').should('exist');
+    cy.get('[data-test=sidenav-notifications]').should('exist');
+    cy.get('[data-test=sidenav-signout]').should('exist');
+    cy.get('[data-test=sidenav-toggle]').should('exist');
+    cy.get('[data-test=nav-top-new-transaction]').should('exist');
+    cy.get('[data-test=nav-top-notifications-link]').should('exist');
+    cy.get('[data-test=nav-public-tab]').should('exist');
+    cy.get('[data-test=nav-contacts-tab]').should('exist');
+    cy.get('[data-test=nav-personal-tab]').should('exist');
+    cy.get('[data-test=transaction-list-filter-date-range-button]').should('exist');
+    cy.get('[data-test=transaction-list-filter-amount-range-button]').should('exist');
+};
 
 describe('Verify all the elements of the websites are existing', () => {
 
@@ -44,7 +64,6 @@ describe('Verify all the elements of the websites are existing', () => {
         cy.get('.PrivateSwitchBase-input-14').should('exist');
         cy.get('[data-test=signup]').should('exist');
         cy.get('[data-test="signin-submit"]').should('exist');
-
         //login with an existing account and then logout
         LOGIN();
         LOGOUT();
@@ -52,7 +71,6 @@ describe('Verify all the elements of the websites are existing', () => {
 
 
     it('TC002 - Sign up page - Verify elements of sign up page', () => {
-
         cy.get('[data-test=signup]').click();
         //verify elements
         cy.get('#firstName').should('exist');
@@ -61,7 +79,6 @@ describe('Verify all the elements of the websites are existing', () => {
         cy.get('#password').should('exist');
         cy.get('#confirmPassword').should('exist');
         cy.get('[data-test=signup-submit]').should('exist');
-
         //login after sign up to verify sign up is working
         var d = new Date();
         var n = d.getTime();
@@ -87,7 +104,30 @@ describe('Verify all the elements of the websites are existing', () => {
         cy.get('.MuiBox-root > .MuiTypography-root').should('be.visible');
         cy.get('[data-test=user-onboarding-next] > .MuiButton-label').click();
         cy.get('[data-test=sidenav-username]').should('be.visible');
+        //logout
+        LOGOUT();
+    });
 
+    it('TC003 - Home page - Verify elements of Home page', () => {
+        //login
+        LOGIN();
+        BODY();
+        //logout
+        LOGOUT();
+    });
+
+    it('TC004 - Contacts page - Verify elements of Contacts page', () => {
+        //login
+        LOGIN();
+        //verify static elements of body
+        BODY();
+        cy.get('[data-test=nav-contacts-tab]').click();
+        cy.contains('Contacts').should('be.visible');
+        cy.get('[data-test=transaction-list-filter-date-range-button]').should('exist');
+        cy.get('[data-test=transaction-list-filter-amount-range-button]').should('exist');
+        cy.get('[data-test=transaction-list-empty-create-transaction-button]').should('exist');
+        cy.get('.MuiGrid-container > :nth-child(1) > svg').should('be.visible');
+        //logout
         LOGOUT();
     });
 
