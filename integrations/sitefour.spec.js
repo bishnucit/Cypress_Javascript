d5bebad23c2fbf716c7039dc4837f2bfff5cff4b
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-10-03 16:36:03 +0530

    Update sitefour.spec.js

--- integrations/sitefour.spec.js
+++ integrations/sitefour.spec.js
@@ -54,7 +54,7 @@ describe('Verify all the elements of the websites are existing', () => {
         cy.visit(testURL,{failOnStatusCode: false});
     });
 
-    it('TC001 - Sign in page - Verify elements of sign in page', () => {
+    it.skip('TC001 - Sign in page - Verify elements of sign in page', () => {
         //verify elements
         cy.get('.makeStyles-logo-3').should('exist');
         cy.get('.MuiTypography-h5').should('exist');
@@ -70,7 +70,7 @@ describe('Verify all the elements of the websites are existing', () => {
     });
 
 
-    it('TC002 - Sign up page - Verify elements of sign up page', () => {
+    it.skip('TC002 - Sign up page - Verify elements of sign up page', () => {
         cy.get('[data-test=signup]').click();
         //verify elements
         cy.get('#firstName').should('exist');
@@ -108,7 +108,7 @@ describe('Verify all the elements of the websites are existing', () => {
         LOGOUT();
     });
 
-    it('TC003 - Home page - Verify elements of Home page', () => {
+    it.skip('TC003 - Home page - Verify elements of Home page', () => {
         //login
         LOGIN();
         BODY();
@@ -116,7 +116,7 @@ describe('Verify all the elements of the websites are existing', () => {
         LOGOUT();
     });
 
-    it('TC004 - Contacts page - Verify elements of Contacts page', () => {
+    it.skip('TC004 - Contacts page - Verify elements of Contacts page', () => {
         //login
         LOGIN();
         //verify static elements of body
@@ -129,6 +129,40 @@ describe('Verify all the elements of the websites are existing', () => {
         cy.get('.MuiGrid-container > :nth-child(1) > svg').should('be.visible');
         //logout
         LOGOUT();
+    });
+
+    it('TC005 - Mine page - Verify elements of Personal page', () => {
+        //login
+        LOGIN();
+        //verify static elements of body
+        BODY();
+        cy.get('[data-test=nav-personal-tab]').click();
+        cy.contains('Personal').should('be.visible');
+        cy.get('[data-test=transaction-list-filter-date-range-button]').should('exist');
+        cy.get('[data-test=transaction-list-filter-amount-range-button]').should('exist');
+        cy.get('[data-test=transaction-list-empty-create-transaction-button]').should('exist');
+        cy.get('.MuiGrid-container > :nth-child(1) > svg').should('be.visible');
+        //logout
+        LOGOUT();
+    });
+
+    it('TC006 - New Transaction page - Verify elements of New Transaction page', () => {
+        //login
+        LOGIN();
+        //verify static elements of body
+        BODY();
+        cy.get('[data-test=nav-top-new-transaction]').click();
+        cy.get('.MuiStepper-root > :nth-child(1)').should('exist');
+        cy.get('.MuiStepper-root > :nth-child(3)').should('exist');
+        cy.get('.MuiStepper-root > :nth-child(5)').should('exist');
+        cy.get('[data-test=user-list-search-input]').should('exist');
+        cy.get('[data-test=user-list-item-t45AiwidW]').should('be.visible');
+        cy.get('[data-test=user-list-item-qywYp6hS0U]').should('be.visible');
+        cy.get('[data-test=user-list-item-bDjUb4ir5O]').should('be.visible');
+        cy.get('[data-test=user-list-item-24VniajY1y]').should('be.visible');
+        cy.get('[data-test=user-list-item-tsHF6_D5oQ]').should('be.visible');
+        //logout
+        LOGOUT();       
     });
 
 });
