1ca4cb9f8cb850855b1c9c0bdc125c03abbdd182
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-10-17 20:36:00 +0530

    Create sitefive.spec.js

---
+++ integrations/api/sitefive.spec.js
@@ -0,0 +1,31 @@
+// sitefive.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+//reference for assertions
+//https://docs.cypress.io/guides/references/assertions#TDD-Assertions
+
+describe('Scenario 1 - Verify all endpoints ', () => {
+
+   it('TC001 - Verify all valid endpoints are giving a 200 response', () => {
+        cy.request({
+            method: 'POST',
+            url: 'https://restful-booker.herokuapp.com/auth',
+            body: {
+                    'username':'admin',
+                    'password':'password123'
+                  },
+            headers: {'content-type': 'application/JSON'}
+
+        }).then(function(response){
+            expect(response.status).to.eq(200);
+            expect(response).to.have.property('headers');
+            expect(response).to.have.property('duration');
+            expect(response.body).have.property('token');
+        });
+    });
+
+});
