ef4bc13041aab661ecadeb65a0cee2fb9a9111b5
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-10-18 17:14:40 +0530

    Update sitefive.spec.js

--- integrations/api/sitefive.spec.js
+++ integrations/api/sitefive.spec.js
@@ -18,14 +18,65 @@ describe('Scenario 1 - Verify all endpoints ', () => {
                     'username':'admin',
                     'password':'password123'
                   },
-            headers: {'content-type': 'application/JSON'}
-
+            headers: {
+                    'content-type': 'application/JSON'
+                    }
         }).then(function(response){
             expect(response.status).to.eq(200);
             expect(response).to.have.property('headers');
             expect(response).to.have.property('duration');
+            expect(response.body).to.not.be.null;
             expect(response.body).have.property('token');
+            expect('token').to.be.a('string');
+        });
+        cy.request({
+            method: 'GET',
+            url: 'https://restful-booker.herokuapp.com/booking',
+            }).then(function(response){
+            expect(response.status).to.eq(200);
+            expect(response).to.have.property('headers');
+            expect(response).to.have.property('duration');
+            expect(response.body).to.not.be.null;
+            expect(response.body).to.be.a('array');
+        });
+        cy.request({
+            method: 'GET',
+            url: 'https://restful-booker.herokuapp.com/booking/1',
+            }).then(function(response){
+            expect(response.status).to.eq(200);
+            expect(response).to.have.property('headers');
+            expect(response).to.have.property('duration');
+            expect(response.body).to.not.be.null;
+            expect(response.body).have.property('bookingdates');
+            expect(response.body).have.property('depositpaid');
+            expect(response.body).have.property('firstname');
+            expect(response.body).have.property('lastname');
+            expect(response.body).have.property('totalprice');
+            expect('firstname').to.be.a('string');
+            expect('lastname').to.be.a('string');
+        });
+        cy.request({
+            method: 'GET',
+            url: 'https://restful-booker.herokuapp.com/booking',
+            body: {
+                    "firstname" : "Jim",
+                    "lastname" : "Brown",
+                    "totalprice" : 111,
+                    "depositpaid" : true,
+                    "bookingdates" : {
+                        "checkin" : "2018-01-01",
+                        "checkout" : "2019-01-01"
+                    },
+                    "additionalneeds" : "Breakfast"
+                  },
+            headers: {
+                    'content-type': 'application/JSON'
+                    }
+            }).then(function(response){
+            expect(response.status).to.eq(200);
+            expect(response).to.have.property('headers');
+            expect(response).to.have.property('duration');
+            expect(response.body).to.not.be.null;
         });
     });
-
 });
